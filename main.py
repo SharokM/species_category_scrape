aef26b14b14bad5aae9ba22ffdc1c85be8e19235
sharok_m <sharok.d.mcdonnaugh@gmail.com>
2026-03-10 12:40:04 +0000

    getting/ finding specific species data

--- main.py
+++ main.py
@@ -26,10 +26,30 @@ def get_categories(url):
   # return the data here
   return data
  
+#  function will extract the class each animal belongs to
+def get_animal(url):
+  soup = get_soup(url)
+  # use the soup.find() function to extract data from a table on the Honey Badger Wikipedia article
+  table = soup.find("table", {"class": "infobox biota"})
+  if not table:
+    return "No class found"
+
+  rows = table.find_all("tr")
+  for row in rows:
+    if "Class:" in row.get_text():
+      # This is the name of the animal class for the specific animal
+      animal_class = row.find("a").contents[0]
+
+  return animal_class
+
 
 category_data = get_categories("https://skillcrush.github.io/web-scraping-endangered-species/")
 
+animal_class = get_animal("https://en.wikipedia.org/wiki/Honey_badger")
+
+
 print(category_data)
+print(animal_class)
 
 # -----------------------------------------------------
 # -----------------------------------------------------
@@ -55,10 +75,15 @@ def get_categories(url):
 
       # empty data dictionary 
   data = {}
-  #ADD CODE - select and extract the mean pay gap here OR status of reported category if change
-
-  categories = soup.find_all("govuk-tag--green")
-  # Return the data here
+  #ADD CODE - select and extract the people according
+  categories = soup.find_all("dl")
+  for category in categories:
+    category_name = category.find("dt").get_text()
+    category_subjects = category.find_all("a") 
+    data[category_name] = category_subjects
+  # return the data here
   return data
 
 category_data = get_categories("https://gender-pay-gap.service.gov.uk/compare-employers/2024")
+
+print(category_data)
